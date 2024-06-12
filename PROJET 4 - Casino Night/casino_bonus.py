d854f398dd06c10398d71fae2893cee054b983de
vividem <victor.demu@gmail.com>
2024-06-12 11:49:40 +0200

    ajout cypher

---
+++ PROJET 4 - Casino Night/casino_bonus.py
@@ -0,0 +1,44 @@
+import random
+
+somme_joueur = 100
+while somme_joueur>0:
+    print("Somme actuelle: "+str(somme_joueur))
+    argent_pari = int(input("\nCombien d'argent souhaitez-vous miser? "))
+    if argent_pari>somme_joueur:
+        print("Vous n'avez pas assez de fonds pour ça, pari annulé.")
+        continue
+    type_pari = input("Voulez-vous parier sur:\n\tA - pair/impair\n\tB - un nombre exact\n\tC - couleur\nA, B ou C?\n")
+    if type_pari.lower()=="a":
+        choix = input("pair ou impair?\n")
+        if choix!="pair" and choix!="impair":
+            print("Je n'ai pas compris, pari annulé.")
+            continue
+    elif type_pari.lower()=="b":
+        choix = int(input("Quel nombre? (entre 1 et 36 inclus)\n"))
+        if choix<0 or choix>36:
+            print("Nombre en dehors des limites. Pari annulé.")
+            continue
+    else:
+        print("Je n'ai pas compris votre demande, pari annulé.")
+        continue
+    print("Les jeux sont faits, rien ne va plus!")
+    res = random.randint(1,36)
+    print("Le "+str(res)+"!")
+    somme_remportee = -argent_pari
+    if type_pari.lower()=="a":
+        if res%2 == 0:
+            if choix == "pair":
+                somme_remportee = argent_pari
+        else:
+            if choix == "impair":
+                somme_remportee = argent_pari
+    else:
+        if choix == res:
+            somme_remportee = 35*argent_pari
+    if somme_remportee>0:
+        print("Pari réussi! Félicitations!")
+    else:
+        print("Perdu...")
+    somme_joueur = somme_joueur + somme_remportee
+
+print("Vous n'avez plus assez d'argent, veuillez quitter le casino.")
---
+++ PROJET 5 - Un challenge crypté/decypher.py
@@ -0,0 +1,39 @@
+def traduire_lettre(lettre, offset):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    # On transforme la lettre en minuscule pour ne pas devoir traiter les majuscules séparément.
+    lettre = lettre.lower()
+    if lettre not in alphabet:
+        # Si jamais la lettre n'est pas dans l'alphabet de base, celle-ci ne change pas.
+        return lettre
+    i = alphabet.index(lettre)
+    # On trouve le nouvel indice et on utilise le modulo pour éviter que ce nouvel indice ne dépasse la longueur de l'alphabet
+    # (le modulo a une propriété qui permet de recommencer à zéro quand on dépasse un nombre:
+    # si je fait a = x%10 et que x est plus petit que 10, a sera égal à x.
+    # Mais si x est plus grand ou égal à 10, a sera égal à x-10 (tant que x est plus petit que 20))
+    new_i = (i+offset)%len(alphabet)
+    return alphabet[new_i]
+
+texte = """Fhfl hvw xqh sduwlh gx whawh, iéolflwdwlrqv.
+Pdokhxuhxvhphqw, fhwwh sduwlh hvw xq slèjh srxu frqwuhu o'dssurfkh xqltxhphqw pdqxhooh.
+Ohv oljqhv gx edv rqw xq dxwuh géfdodjh txh fhv oljqhv flv.
+Lo idxw wurxyhu ohxu géfdodjh fdu fh vrqw hoohv txl frqwlhqqhqw oh yudl lqglfh.
+Ev wrzkvj grj rkkvekzfe rlo 4 czxevj rl uvjjlj, vccvj jlzmvek le rlkiv uétrcrxv, drzj jfek zelkzcvj.
+Cvj 4 czxevj jlzmrekvj jfek cvj gclj zdgfikrekvj:
+Dfe givdzvi vjk lev efkv uv dljzhlv.
+Urej dfe uvlozèdv fe gvlk kiflmvi uv c'fi.
+Cv tfigj yldrze vjk wrzk à 60% uv dfe kifzjzèdv.
+Dfe kflk vjk le avl uv jftzéké u'fizxzev tyzefzjv.
+"""
+offset = 1
+while True:
+    # On crée un nouveau texte vide dans lequel on va ajouter toutes les lettres décalées
+    new_text = ""
+    for c in texte:
+        # On décale chaque lettre d'un même décalage "offset" et on les rajoute au nouveau texte
+        new_text += traduire_lettre(c, offset)
+    # On affiche le nouveau texte final
+    print(new_text)
+    # Le input sert ici juste à pauser la boucle. L'utilisateur peut laisser le code continuer en appuyant sur enter
+    input()
+    # On augmente l'offset de 1 pour essayer le décalage suivant si jamais on a pas encore trouvé le bon décalage
+    offset += 1
